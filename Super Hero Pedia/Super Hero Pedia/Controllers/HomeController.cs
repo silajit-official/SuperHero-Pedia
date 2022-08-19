a8e77b455a9468f9408f95aff7a373c58cb94c77
silajit.saha <silajit.saha@BHUKRK393724L>
2022-08-19 18:45:48 +0530

    Added Header

--- Super Hero Pedia/Super Hero Pedia/Controllers/HomeController.cs
+++ Super Hero Pedia/Super Hero Pedia/Controllers/HomeController.cs
@@ -6,12 +6,6 @@ namespace Super_Hero_Pedia.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
@@ -23,10 +17,6 @@ namespace Super_Hero_Pedia.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+       
     }
 }
