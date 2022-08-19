2ad7572c23b557fb12310e9bae18281a2aa4da2e
silajit.saha <silajit.saha@BHUKRK393724L>
2022-08-19 17:32:10 +0530

    Add

---
+++ Super Hero Pedia/Super Hero Pedia/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Super_Hero_Pedia.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
